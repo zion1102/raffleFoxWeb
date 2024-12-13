34b2086f1a4c86a26e150a84ab032cd750fb4ec8
Zion Henry <zionhenry@gmail.com>
2024-12-13 16:32:46 -0400

    apple

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,19 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
 
 // Replace with your actual values
 const TEAM_ID = 'Y5N3U7CU4N'; // Your Apple Developer Team ID
 const KEY_ID = '3VG9HSG4ZZ'; // Your Key ID from Apple Developer Portal
 const CLIENT_ID = 'com.example.raffle-Fox.service'; // Your Service ID
 
-// Path to your private key file downloaded from Apple Developer
-const PRIVATE_KEY_PATH = '/Users/zionhenry/dev_folder/raffleFoxWeb/AuthKey_3VG9HSG4ZZ.p8';
+// Private key from Apple Developer
+const privateKey = `-----BEGIN PRIVATE KEY-----
+MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgHdU4A3xoFSP/ajcx
+6pjM3j3cR8fSRVy2bh1uxXQyZXmgCgYIKoZIzj0DAQehRANCAAQu9eSZIX+nFyjg
+t6MaKwNMCWnsgSmiwm3SOKbtxWGpxX8cPGpMp1u6AF0REic88WtDZb3aaCpxR7QJ
+zQvX5W1k
+-----END PRIVATE KEY-----`;
 
 /**
  * Generates a client secret for Apple Sign-In using JWT.
@@ -17,8 +21,6 @@ const PRIVATE_KEY_PATH = '/Users/zionhenry/dev_folder/raffleFoxWeb/AuthKey_3VG9H
  */
 function generateClientSecret() {
   try {
-    const privateKey = fs.readFileSync(PRIVATE_KEY_PATH, 'utf8');
-
     const payload = {
       iss: TEAM_ID,
       iat: Math.floor(Date.now() / 1000),
